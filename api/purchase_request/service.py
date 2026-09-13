19b10b189d7b3741addf92e8dd73575e5027b643
gunjou <gjoefandi@gmail.com>
2026-09-14 07:14:27 +0800

    fix(auth): update admin password example in login model for clarity fix(purchase_request): adjust validation logic for editing purchase request status

--- api/purchase_request/service.py
+++ api/purchase_request/service.py
@@ -341,7 +341,8 @@ def update_purchase_request_service(id_request: int, id_pegawai: int, body: dict
 
     status = request_data["status"]
 
-    if id_pegawai != None and status not in ("REQUESTED", "REJECTED"):
+    # if id_pegawai != None and status not in ("REQUESTED", "REJECTED"):
+    if id_pegawai != None:
         raise ValidationError(
             f"Pengajuan dengan status {status} tidak dapat diedit."
         )
